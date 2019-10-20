13ab786e26c00ab2a1d95dcfe91b28e8360a4967
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-20 15:39:23 -0400

    push

---
+++ FallingObjectTobiV2/FallingObjectTobiV2/FalingObjectForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace FallingObjectTobiV2
+{
+    partial class FalingObjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.mnuFile = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(149, 108);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(12, 58);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(379, 17);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "Enter the time (in secondes) since you released the object:";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(433, 58);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 22);
+            this.txtTime.TabIndex = 2;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(12, 153);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(292, 17);
+            this.lblHeight.TabIndex = 3;
+            this.lblHeight.Text = "The height of the object above the ground is:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(383, 153);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(46, 17);
+            this.lblAnswer.TabIndex = 4;
+            this.lblAnswer.Text = "label1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuFile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuFile.Location = new System.Drawing.Point(0, 0);
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(593, 28);
+            this.mnuFile.TabIndex = 5;
+            this.mnuFile.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(46, 24);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(224, 26);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // FalingObjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 246);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.mnuFile);
+            this.MainMenuStrip = this.mnuFile;
+            this.Name = "FalingObjectForm";
+            this.Text = "Falling object Tobi";
+            this.mnuFile.ResumeLayout(false);
+            this.mnuFile.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.MenuStrip mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ FallingObjectTobiV2/FallingObjectTobiV2/FalingObjectForm.cs
@@ -0,0 +1,62 @@
+﻿/*
+   * Created by: Tobi adebayo
+   * Created on: 20-10-2019
+   * Created for: ICS3U Programming
+   * Assignment – #2 - Falling object
+   * This program calculates the height of and object after being dropped
+  */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjectTobiV2
+{
+    public partial class FalingObjectForm : Form
+    {
+        public FalingObjectForm()
+        {
+            InitializeComponent();
+            //hide the labels
+            lblAnswer.Hide();
+            lblHeight.Hide();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+            //Make and exit buton
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            lblHeight.Show();
+            lblAnswer.Show();
+
+            // declare local variables
+            double time, answer;
+
+            // Convert from string to double
+            time = double.Parse(txtTime.Text);
+
+            // Calculation for time
+            answer = 100 - (0.5 * 9.81 * Math.Pow(time, 2));
+
+            // Add the "M" at the end
+            lblAnswer.Text = Convert.ToString(answer) + "m";
+
+            //Put it already hit the ground after it has hit the ground
+            if (time > Math.Sqrt((100/9.81)))
+            {
+                lblAnswer.Text = "It already hit the ground!";
+
+            }
+
+        }
+    }
+}
