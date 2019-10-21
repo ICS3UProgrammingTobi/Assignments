a0dd69f1663e2fbe4cb1bc17ae15375d5aa89bd8
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-21 12:00:03 -0400

    push

--- FallingObjectTobiV2/FallingObjectTobiV2/FalingObjectForm.Designer.cs
+++ FallingObjectTobiV2/FallingObjectTobiV2/FalingObjectForm.Designer.cs
@@ -41,9 +41,10 @@
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(149, 108);
+            this.btnCalculate.Location = new System.Drawing.Point(112, 88);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.Size = new System.Drawing.Size(78, 19);
             this.btnCalculate.TabIndex = 0;
             this.btnCalculate.Text = "Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
@@ -52,34 +53,38 @@
             // lblTime
             // 
             this.lblTime.AutoSize = true;
-            this.lblTime.Location = new System.Drawing.Point(12, 58);
+            this.lblTime.Location = new System.Drawing.Point(9, 47);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTime.Name = "lblTime";
-            this.lblTime.Size = new System.Drawing.Size(379, 17);
+            this.lblTime.Size = new System.Drawing.Size(282, 13);
             this.lblTime.TabIndex = 1;
             this.lblTime.Text = "Enter the time (in secondes) since you released the object:";
             // 
             // txtTime
             // 
-            this.txtTime.Location = new System.Drawing.Point(433, 58);
+            this.txtTime.Location = new System.Drawing.Point(325, 47);
+            this.txtTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtTime.Name = "txtTime";
-            this.txtTime.Size = new System.Drawing.Size(100, 22);
+            this.txtTime.Size = new System.Drawing.Size(76, 20);
             this.txtTime.TabIndex = 2;
             // 
             // lblHeight
             // 
             this.lblHeight.AutoSize = true;
-            this.lblHeight.Location = new System.Drawing.Point(12, 153);
+            this.lblHeight.Location = new System.Drawing.Point(9, 124);
+            this.lblHeight.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblHeight.Name = "lblHeight";
-            this.lblHeight.Size = new System.Drawing.Size(292, 17);
+            this.lblHeight.Size = new System.Drawing.Size(220, 13);
             this.lblHeight.TabIndex = 3;
             this.lblHeight.Text = "The height of the object above the ground is:";
             // 
             // lblAnswer
             // 
             this.lblAnswer.AutoSize = true;
-            this.lblAnswer.Location = new System.Drawing.Point(383, 153);
+            this.lblAnswer.Location = new System.Drawing.Point(287, 124);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAnswer.Name = "lblAnswer";
-            this.lblAnswer.Size = new System.Drawing.Size(46, 17);
+            this.lblAnswer.Size = new System.Drawing.Size(35, 13);
             this.lblAnswer.TabIndex = 4;
             this.lblAnswer.Text = "label1";
             // 
@@ -90,7 +95,8 @@
             this.mniFile});
             this.mnuFile.Location = new System.Drawing.Point(0, 0);
             this.mnuFile.Name = "mnuFile";
-            this.mnuFile.Size = new System.Drawing.Size(593, 28);
+            this.mnuFile.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.mnuFile.Size = new System.Drawing.Size(445, 24);
             this.mnuFile.TabIndex = 5;
             this.mnuFile.Text = "menuStrip1";
             // 
@@ -99,21 +105,21 @@
             this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mniExit});
             this.mniFile.Name = "mniFile";
-            this.mniFile.Size = new System.Drawing.Size(46, 24);
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
             this.mniFile.Text = "File";
             // 
             // mniExit
             // 
             this.mniExit.Name = "mniExit";
-            this.mniExit.Size = new System.Drawing.Size(224, 26);
+            this.mniExit.Size = new System.Drawing.Size(93, 22);
             this.mniExit.Text = "Exit";
             this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
             // 
             // FalingObjectForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 246);
+            this.ClientSize = new System.Drawing.Size(445, 200);
             this.Controls.Add(this.lblAnswer);
             this.Controls.Add(this.lblHeight);
             this.Controls.Add(this.txtTime);
@@ -121,6 +127,7 @@
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.mnuFile);
             this.MainMenuStrip = this.mnuFile;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FalingObjectForm";
             this.Text = "Falling object Tobi";
             this.mnuFile.ResumeLayout(false);
